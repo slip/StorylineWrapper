94de6b4ddf7cef7596e9de3e811f406e1e7eaeef
Ian Kennedy <exposure@gmail.com>
2015-04-29 10:17:49 -0400

    Initial Commit
    Quick utility to add a custom wrapper to Storyline courses. Replaces
    frame.swf and makes changes to html files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class CaptivateFixer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.directoryBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.hasResourcesCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // directoryBtn
+            // 
+            this.directoryBtn.Location = new System.Drawing.Point(286, 45);
+            this.directoryBtn.Name = "directoryBtn";
+            this.directoryBtn.Size = new System.Drawing.Size(124, 23);
+            this.directoryBtn.TabIndex = 1;
+            this.directoryBtn.Text = "Fix My Course";
+            this.directoryBtn.UseVisualStyleBackColor = true;
+            this.directoryBtn.Click += new System.EventHandler(this.directoryBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Click below to choose the directory of your Captivate course. We\'ll handle the re" +
+    "st.";
+            // 
+            // hasResourcesCheckBox
+            // 
+            this.hasResourcesCheckBox.AutoSize = true;
+            this.hasResourcesCheckBox.Location = new System.Drawing.Point(167, 51);
+            this.hasResourcesCheckBox.Name = "hasResourcesCheckBox";
+            this.hasResourcesCheckBox.Size = new System.Drawing.Size(99, 17);
+            this.hasResourcesCheckBox.TabIndex = 3;
+            this.hasResourcesCheckBox.Text = "Resources Tab";
+            this.hasResourcesCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // CaptivateFixer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 92);
+            this.Controls.Add(this.hasResourcesCheckBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.directoryBtn);
+            this.Name = "CaptivateFixer";
+            this.Text = "Storyline Wrapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button directoryBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox hasResourcesCheckBox;
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using WindowsFormsApplication1.Properties;
+
+namespace WindowsFormsApplication1
+{
+    public partial class CaptivateFixer : Form
+    {
+        // 
+        string folderPath = "";
+        string docPath = "";
+        string destinationPath = "";
+
+        public CaptivateFixer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void directoryBtn_Click(object sender, EventArgs e)
+        {
+
+            // get the folder we want to search and update the folderPath
+            FolderBrowserDialog folderBrowserDialog1 = new FolderBrowserDialog();
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                folderPath = folderBrowserDialog1.SelectedPath;
+                docPath = folderPath + "\\story.html";
+                destinationPath = folderPath + "\\story_content\\frame.swf";
+
+            }
+
+            // check to make sure directory is valid, throw error if not
+            if (!File.Exists(docPath))
+            {
+                MessageBox.Show("This doesn't seem to be a StoryLine course. Please choose the directory of your course output folder.");
+            }
+            // copy frame.swf into directory/story_content
+            else
+            {
+                // get correct frameswf
+                if (hasResourcesCheckBox.Checked)
+                {
+                    File.WriteAllBytes(destinationPath, Resources.frame_wResources);
+                }
+                else
+                {
+                    File.WriteAllBytes(destinationPath, Resources.frame_noResources);
+                }
+            }
+
+            // replace lines in story.html et al
+        }
+
+        private void CopyResource(string resourceName, string file)
+        {
+            using (Stream resource = GetType().Assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resource == null)
+                {
+                    throw new ArgumentException("No such resource", "resourceName");
+                }
+                using (Stream output = File.OpenWrite(file))
+                {
+                    resource.CopyTo(output);
+                }
+            }
+        }
+    }
+}
